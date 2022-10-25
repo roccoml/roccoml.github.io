0dba62e09699d7b54cd392cd6aff38eefdec5dad
roccoml <rola4344@colorado.edu>
2022-10-25 10:30:26 -0600

    ica10

--- java/assignment9.js
+++ java/assignment9.js
@@ -34,7 +34,8 @@ for (const image of images) {
       btn.setAttribute('class','light');
       btn.textContent = 'Lighten';
       overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
-    } else {
+    } 
+    else {
       btn.setAttribute('class','dark');
       btn.textContent = 'Darken';
       overlay.style.backgroundColor = 'rgba(0,0,0,0)';
