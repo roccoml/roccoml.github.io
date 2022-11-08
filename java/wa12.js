2c826bf7ca1b44f6db1c4da4e7f53e5cc0e5fd77
roccoml <rola4344@colorado.edu>
2022-11-07 22:10:24 -0700

    wa12

---
+++ java/wa12.js
@@ -0,0 +1,10 @@
+const button = document.querySelector('.button');
+const output = document.querySelector('.output');
+let phone_content = document.querySelector('.phone');
+
+button.addEventListener('click', updateOutput);
+
+function updateOutput() {
+    output.textContent = phone_content.value;
+}
+
